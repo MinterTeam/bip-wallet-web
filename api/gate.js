9be440bbdf7ea5e156fee01bf6adfa364ae73054
shrpne <shrpne@gmail.com>
2019-08-21 20:21:55 +0300

    WA-217: show payload in the tx preview

--- api/gate.js
+++ api/gate.js
@@ -2,9 +2,9 @@ import MinterApi from "minter-js-sdk/src/api";
 import PostTx from 'minter-js-sdk/src/api/post-tx';
 import EstimateCoinSell from 'minter-js-sdk/src/api/estimate-coin-sell';
 import EstimateCoinBuy from 'minter-js-sdk/src/api/estimate-coin-buy';
-import {GATE_API_HOST, NETWORK, MAINNET, TESTNET} from '~/assets/variables';
+import {GATE_API_URL, CHAIN_ID} from '~/assets/variables';
 
-const minterApi = new MinterApi({apiType: 'gate', baseURL: GATE_API_HOST, chainId: NETWORK === MAINNET ? 1 : 2});
+const minterApi = new MinterApi({apiType: 'gate', baseURL: GATE_API_URL, chainId: CHAIN_ID});
 
 export const postTx = new PostTx(minterApi);
 
